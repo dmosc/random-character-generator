6913657a1d1ce2588eb5e54f2d8cf1230fb3e065
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-16 19:06:21 -0500

    App handles card fetching and card displaying inside Card Component and SeenCards component. Main state handler.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { GET_RANDOM_CHARACTER } from "./components/queries";
+import SeenCards from "./components/SeenCards";
+import Card from "./components/Card";
+import Button from "./components/Button";
+import { ApolloConsumer } from "react-apollo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    seen: new Map(),
+    current: {}
+  };
+
+  render() {
+    const { current, seen } = this.state;
+    return (
+      <ApolloConsumer>
+        {client => (
+          <div className='App'>
+            <div className='container'>
+              <div className='row'>
+                <div className='col-5'>
+                  <h6>
+                    Have you seen them all?{" "}
+                    <span role='img' aria-label='eyes'>
+                      👀
+                    </span>
+                  </h6>
+                  <SeenCards seen={seen} onClick={this.handleCardSelect} />
+                </div>
+                <div className='col-7'>
+                  {Object.keys(current).length > 0 && (
+                    <Card character={current || {}} />
+                  )}
+                  <Button onClick={this.handleCardUpdate} client={client} />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </ApolloConsumer>
+    );
+  }
+
+  handleCardUpdate = async client => {
+    const seen = new Map(this.state.seen);
+    const randId = Math.trunc(Math.random() * (400 - 1) + 1);
+
+    if (seen.has(randId)) {
+      const current = seen.get(randId);
+      //If info has been already requested, there's no need to query again
+      return this.setState({ current }); // card was already fetched
+    }
+
+    const {
+      data: { character: current }
+    } = await client.query({
+      query: GET_RANDOM_CHARACTER,
+      variables: { randId }
+    });
+
+    seen.set(randId.toString(), current); //add to map
+    return this.setState({ seen, current });
+  };
+
+  handleCardSelect = id => {
+    const { seen } = this.state;
+    const current = { ...seen.get(id.toString()) };
+
+    return this.setState({ current });
+  };
 }
 
 export default App;

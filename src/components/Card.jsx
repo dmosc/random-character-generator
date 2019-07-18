92b7f077f833ffff939ed83a52072dac9cc1672a
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-18 01:40:51 -0500

    Added logic handling inside Card component as well as id calculation.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 const Card = props => {
   const character = { ...props.character };
-
   return (
     <div className='card mb-3'>
       <div className='row no-gutters'>
@@ -17,11 +16,14 @@ const Card = props => {
         <div className='col-md-7 p-3'>
           <h5 className='card-title d-flex justify-content-between mb-0'>
             <u>{character && character.name}</u>
-            <span className='badge badge-pill badge-info'>{character.id}</span>
+            <span className='badge badge-pill badge-info'>
+              {character._id.slice(character._id.length - 3)}
+            </span>
           </h5>
+          {/* get rid of unnecessary attributes */}
           {delete character.image &&
             delete character.name &&
-            delete character.id}
+            delete character._id}
           <div
             className='card-body'
             style={{ maxHeight: "200px", overflowY: "scroll" }}
@@ -29,9 +31,11 @@ const Card = props => {
             <div className='card-text'>
               <ul className='list-group list-group-flush'>
                 {character &&
-                  Object.keys(character).map(key => (
-                    <li key={key} className='list-group-item'>
-                      {key}: {character[key].name || character[key]}
+                  Object.keys(character).map((key, i) => (
+                    <li key={i} className='list-group-item'>
+                      {key}:{" "}
+                      {(character[key] && character[key].name) ||
+                        character[key]}
                     </li>
                   ))}
               </ul>

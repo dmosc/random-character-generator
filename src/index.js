e8c7e22ca0d85e82151af9231d764fde2bb2b625
Oscar Rodríguez <oscardavidrodriguezm@gmail.com>
2019-07-18 01:39:14 -0500

    Added graphql localhost as the API endpoint.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import "./index.css";
 import App from "./App";
 
 const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql",
+  uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache({
     addTypename: false
   })
